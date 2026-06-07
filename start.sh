b137d19552b42cc6b032a8ef913969c1b7824f0e
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2026-06-07 21:19:13 +0600

    fix: prevent envsubst from truncating nginx.conf before reading

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
+mv /tmp/default.conf /etc/nginx/conf.d/default.conf
 nginx
 uvicorn main:app --host 0.0.0.0 --port 8001
